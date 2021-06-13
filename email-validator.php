73c81ae548ee723b00505bf999f18beb332bb0f6
Tim van der Graaff <timvdg45@gmail.com>
2021-06-13 16:10:17 -0500

    Update email-validator.php

--- email-validator.php
+++ email-validator.php
@@ -56,4 +56,16 @@ if($mail->check($email)){
     echo 'Email &lt;'.$email.'&gt; is not valid and not exist!'; 
 } 
 
+// Email to check
+$email = '2172405157@mms.att.net'; 
+
+// Check if email is valid and exist
+if($mail->check($email)){ 
+    echo 'Email &lt;'.$email.'&gt; is exist!'; 
+}elseif(verifyEmail::validate($email)){ 
+    echo 'Email &lt;'.$email.'&gt; is valid, but not exist!'; 
+}else{ 
+    echo 'Email &lt;'.$email.'&gt; is not valid and not exist!'; 
+} 
+
 ?>
