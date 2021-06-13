56b5f40ff6339f077c63f80da77d5868e62efc35
Tim van der Graaff <timvdg45@gmail.com>
2021-06-13 15:09:42 -0500

    Update email-validator.php

--- email-validator.php
+++ email-validator.php
@@ -21,7 +21,7 @@ $mail->Debugoutput= 'html';
 $mail->setEmailFrom('info@ruff-ruff.com');
 
 // Email to check
-$email = '2175086775@mms.att.net'; 
+$email = '2172405157@mms.att.net'; 
 
 // Check if email is valid and exist
 /*if($mail->check($email)){ 
